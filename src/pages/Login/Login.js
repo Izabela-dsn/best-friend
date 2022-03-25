3c9ebdb63d1fe428defac299be11720fdd60f85e
Izabela-dsn <izabeladasilva10@gmail.com>
2022-03-25 11:45:21 -0300

    add function authentication and connection with db

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,12 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate  } from "react-router-dom";
+import api from "../../services/api";
 
-import logoFacebook from '../../assets/logos_facebook.svg'
-import logoGoogle from '../../assets/logos_google.svg'
+import logoFacebook from "../../assets/logos_facebook.svg";
+import logoGoogle from "../../assets/logos_google.svg";
 
 import "./style.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    try {
+      api.get("/profile").then((response) => {
+        const info = response.data;
+        info.users.map((user)=>{
+          setUsers(user)
+        })
+      });
+    } catch {
+      window.alert("Oh no");
+    }
+  }, []);
+
+  function auth(e){
+    e.preventDefault();
+    console.log(users)
+    if(users.email === email || users.password === password){
+      navigate(`/user`, {detail: users.id})
+    }
+  }
+  
   return (
     <div className="login">
       <header>
@@ -19,18 +46,20 @@ const Login = () => {
           <h1>Login</h1>
           <fieldset>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="exemplo@email.com"/>
+            <input type="email" id="email" placeholder="exemplo@email.com" onChange={(e)=>{setEmail(e.target.value)}}/>
           </fieldset>
 
           <fieldset>
             <label htmlFor="password">Senha</label>
-            <input type="password" id="password" />
+            <input type="password" id="password" onChange={(e)=>{setPassword(e.target.value)}}/>
           </fieldset>
           <a href="/" className="forgot-password">
             Esqueci minha senha
           </a>
 
-          <Link to="/user" className="a">Entrar</Link>
+          <button className="a" onClick={auth}>Entrar</button>
+          {/*<Link to="#" className="a" >
+            Entrar </Link>*/}
         </form>
         <div className="signIn-socialMedia">
           <h2>Entrar pelo</h2>
