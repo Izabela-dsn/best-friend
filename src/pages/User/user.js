8c964bf98c40782b9141e219ee0079c201a14725
Izabela-dsn <izabeladasilva10@gmail.com>
2022-03-25 11:45:57 -0300

    first version of conection with db

--- src/pages/User/user.js
+++ src/pages/User/user.js
@@ -1,15 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 import EventCard from "../../components/cardsEvents/event";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
-import "./style.css";
 import vacinasRemediosIcon from "../../assets/vacinaRemedios.svg";
 import examIcon from "../../assets/estetoscopio.svg";
 import vetIcon from "../../assets/clinica.svg";
 import cirurgiaIcon from "../../assets/cirurgias.svg";
 import CardInsertData from "../../components/cardInsertData";
+import "./style.css";
 
 const User = () => {
+  let texto = `{
+    "nome": "Fulano",
+    "idade": 90,
+    "filmes_preferidos": [ "Pulp Fiction", "Clube da Luta" ],
+    "contatos": {
+        "telefone": "(11) 91111-2222",
+        "emails": [ "fulano@gmail.com", "fulano@yahoo.com" ]
+    }
+}`;
+  // se o seu browser não suporta a template string acima, use a linha abaixo
+  //let texto = '{ "nome": "Fulano", "idade": 90, "filmes_preferidos": [ "Pulp Fiction", "Clube da Luta" ], "contatos": { "telefone": "(11) 91111-2222", "emails": [ "fulano@gmail.com", "fulano@yahoo.com" ] } }';
+
+  let json = JSON.parse(texto);
+  // imprimir nome
+  //console.log(json.nome);
+  // imprimir filmes
+  //json.filmes_preferidos.forEach((filme) => console.log(filme));
+  // imprimir contatos
+  for (let [tipo, contato] of Object.entries(json.contatos)) {
+    //console.log(tipo, "=", Array.isArray(contato) ? contato.join() : contato);
+  }
+
+  ////////////////////////////////////////////////////////////
+  useEffect(() => {
+    buscarProfile();
+  }, []);
+
   const [categories, setCategories] = useState(
     " ",
     "medicineVaccine",
@@ -26,16 +55,48 @@ const User = () => {
     "formSurgeries"
   );
 
+  const [usersProfile, setUsersProfile] = useState([]);
+  const petProfile = [];
+
+  async function buscarProfile() {
+    try {
+      api.get("/profile").then((response) => {
+        const info = response.data;
+        setUsersProfile(info.users);
+      });
+    } catch {
+      window.alert("Oh no");
+    }
+  }
+
+  try{
+    for(let [index, user] of Object.entries(usersProfile)){
+      user.pet.forEach(element => {
+        petProfile.push(element)
+      });
+    }
+  }catch
+  {
+    window.alert("Não foi possivel recuperar informações do banco")
+  }
+  //console.log(petProfile)
+
   return (
     <div className="profile">
       <Header />
       <main>
-        <h1>Olá, nome</h1>
+        {usersProfile
+          ? usersProfile.map((user) => <h1 key={user.id}>Olá, {user.name}</h1>)
+          : null}
         <div className="pets">
           <div className="pet">
-            <button>
-              <p>Nina</p>
-            </button>
+            {petProfile
+              ? petProfile.map((pet) => (
+                  <button key={pet.id}>
+                    <p>{pet.name}</p>
+                  </button>
+                ))
+              : null}
           </div>
           <div className="pet">
             <button>
@@ -46,11 +107,14 @@ const User = () => {
 
         <section className="about">
           <hr />
-          <div className="about-basic-information">
-            <p className="pet-name">Nome - Nina</p>
-            <p className="pet-years">Idade - 11 anos</p>
-            <p className="pet-weight">Peso - 8 KG</p>
-          </div>
+          {petProfile.map(pet => (
+              <div key={pet.id} className="about-basic-information">
+                <p className="pet-name">Nome - {pet.name}</p>
+                <p className="pet-years">Idade - {pet.years} anos</p>
+                <p className="pet-weight">Peso - {pet.weight} KG</p>
+              </div>
+            ) 
+          )}
 
           <div className="medical-history">
             <section>
@@ -64,9 +128,16 @@ const User = () => {
 
               {categories === "medicineVaccine" && (
                 <div className="vaccines-medicines">
-                  <ul></ul>
-                  <button onClick={() => setOpenForms("formMedicineVaccine")}>
-                    Add
+                  <ul className="list">
+                    {petProfile ? petProfile.map(pet=>{
+                        <EventCard></EventCard>
+                    }): <p>Não foi possivel carregas os dados</p>}
+                  </ul>
+                  <button
+                    className="btnForms"
+                    onClick={() => setOpenForms("formMedicineVaccine")}
+                  >
+                    Adicionar
                   </button>
                   {openForms === "formMedicineVaccine" && (
                     <CardInsertData type={categories} />
@@ -86,8 +157,49 @@ const User = () => {
 
               {categories === "exams" && (
                 <div className="exams">
-                  <ul></ul>
-                  <button onClick={() => setOpenForms("formExams")}>Add</button>
+                  <ul className="list">
+                    <EventCard
+                      locationNameOrType="Clinica Vet"
+                      names="Ultrasom"
+                      time="12 de Julho"
+                    ></EventCard>
+                    <EventCard
+                      locationNameOrType="Clinica Vet"
+                      names="Ultrasom"
+                      time="12 de Julho"
+                    ></EventCard>
+                    <EventCard
+                      locationNameOrType="Clinica Vet"
+                      names="Ultrasom"
+                      time="12 de Julho"
+                    ></EventCard>
+                    <EventCard
+                      locationNameOrType="Clinica Vet"
+                      names="Ultrasom"
+                      time="12 de Julho"
+                    ></EventCard>
+                    <EventCard
+                      locationNameOrType="Clinica Vet"
+                      names="Ultrasom"
+                      time="12 de Julho"
+                    ></EventCard>
+                    <EventCard
+                      locationNameOrType="Clinica Vet"
+                      names="Ultrasom"
+                      time="12 de Julho"
+                    ></EventCard>
+                    <EventCard
+                      locationNameOrType="Clinica Vet"
+                      names="Ultrasom"
+                      time="12 de Julho"
+                    ></EventCard>
+                  </ul>
+                  <button
+                    className="btnForms"
+                    onClick={() => setOpenForms("formExams")}
+                  >
+                    Adicionar
+                  </button>
                   {openForms === "formExams" && (
                     <CardInsertData type={categories} />
                   )}
@@ -106,9 +218,12 @@ const User = () => {
 
               {categories === "veterinary" && (
                 <div className="veterinary">
-                  <ul></ul>
-                  <button onClick={() => setOpenForms("formVeterinary")}>
-                    Add
+                  <ul className="list"></ul>
+                  <button
+                    className="btnForms"
+                    onClick={() => setOpenForms("formVeterinary")}
+                  >
+                    Adicionar
                   </button>
                   {openForms === "formVeterinary" && (
                     <CardInsertData type={categories} />
@@ -127,13 +242,16 @@ const User = () => {
 
             {categories === "surgeries" && (
               <div className="surgeries">
-                <ul></ul>
-                <button onClick={() => setOpenForms("formSurgeries")}>
-                    Add
-                  </button>
-                  {openForms === "formSurgeries" && (
-                    <CardInsertData type={categories} />
-                  )}
+                <ul className="list"></ul>
+                <button
+                  className="btnForms"
+                  onClick={() => setOpenForms("formSurgeries")}
+                >
+                  Adicionar
+                </button>
+                {openForms === "formSurgeries" && (
+                  <CardInsertData type={categories} />
+                )}
               </div>
             )}
           </div>
